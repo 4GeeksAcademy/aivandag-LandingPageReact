3c1673956c2b27c8d5099e5615c294f520cea20b
aivandag <aivan.dev7@gmail.com>
2025-04-17 23:18:43 +0000

    The Last React

--- src/js/Navbar.jsx
+++ src/js/Navbar.jsx
@@ -1,9 +1,9 @@
 const Navbar = () => {
   return (
   
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Start Bootstrap</a>
+          <a className="navbar-brand text-light" href="#">Start Bootstrap</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
             data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
             aria-expanded="false" aria-label="Toggle navigation">
@@ -11,10 +11,10 @@ const Navbar = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav ms-auto">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-              <a className="nav-link" href="#">About</a>
-              <a className="nav-link" href="#">Service</a>
-              <a className="nav-link disabled" aria-disabled="true">Contact</a>
+              <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
+              <a className="nav-link text-secondary" href="#">About</a>
+              <a className="nav-link text-secondary" href="#">Service</a>
+              <a className="nav-link disabled text-secondary" aria-disabled="true">Contact</a>
             </div>
           </div>
         </div>
